a33eba17106dbcf9df420316ab10934ebfd412d7
Fredrik Norén <fredrik.jw.noren@gmail.com>
2023-02-22 13:45:22 +0100

    Remove self_update

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,3 @@
-use self_update::cargo_crate_version;
-
 fn main() {
-    println!("Hello, world {}!", cargo_crate_version!());
-    if let Err(err) = update() {
-        println!("Update failed: {:?}", err);
-    }
-}
-
-fn update() -> Result<(), Box<dyn std::error::Error>> {
-    let status = self_update::backends::github::Update::configure()
-        .repo_owner("FredrikNoren")
-        .repo_name("release_test")
-        .bin_name("github")
-        .show_download_progress(true)
-        .current_version(cargo_crate_version!())
-        .build()?
-        .update()?;
-    println!("Update status: `{}`!", status.version());
-    Ok(())
+    println!("Hello, world!");
 }
